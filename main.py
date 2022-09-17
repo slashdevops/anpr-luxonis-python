245424262bbce09769a5172993a9069cd90c9386
Christian González Di Antonio <christiangda@gmail.com>
2022-09-17 18:44:19 +0200

    feat: improve args flags

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ import numpy as np
 from depthai_sdk import FPSHandler
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--debug", action="store_true", default=True, help="Debug mode")
-parser.add_argument("-cam", "--camera", action="store_true", help="Use DepthAI 4K RGB camera for inference (conflicts with -vid)")
-parser.add_argument("-vid", "--video", type=str, help="Path to video file to be used for inference (conflicts with -cam)")
+parser.add_argument("-d", "--debug", default=True, help="Debug mode")
+parser.add_argument("-cam", "--camera", default=False, help="Use DepthAI 4K RGB camera for inference (conflicts with -vid)")
+parser.add_argument("-vid", "--video", type=argparse.FileType("r", encoding="UTF-8"), help="Path to video file to be used for inference (conflicts with -cam)")
 parser.add_argument("-nn", "--nn-blob-model", type=argparse.FileType("r", encoding="UTF-8"), required=True, help="Set path of the blob (NN model)")
 args = parser.parse_args()
 
@@ -132,7 +132,7 @@ lic_last_seq = 0
 if args.camera:
     fps = FPSHandler()
 else:
-    cap = cv2.VideoCapture(str(Path(args.video).resolve().absolute()))
+    cap = cv2.VideoCapture(str(Path(args.video.name).resolve().absolute()))
     fps = FPSHandler(cap)
 
 
